294559419d42d7565aa44c39206041b99ac1f9b5
div <div.pat499@gmail.com>
2021-07-15 18:57:11 -0400

    Removed extra libs

--- app/src/main/java/com/example/gauntlet/Level.java
+++ app/src/main/java/com/example/gauntlet/Level.java
@@ -5,11 +5,9 @@ import android.graphics.PointF;
 import android.util.Log;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 class Level {
 
