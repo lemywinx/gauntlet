aab6338ae907add05f1ba40363ea3a3c590b609c
div <div.pat499@gmail.com>
2021-07-27 10:39:08 -0400

    Removed extra files from the root folder

--- DoorSpawnComponent.java
+++
@@ -1,9 +0,0 @@
-package com.example.gauntlet;
-
-public class DoorSpawnComponent implements SpawnComponent{
-    @Override
-    public void spawn(Transform playerTransform, Transform t) {
-        // spawn key (currently spawns at x=screen bounds, y=0)
-        t.setLocation(t.getmScreenSize().x/2, 0);
-    }
-}
